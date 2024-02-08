3e65b50c910877c84483797ab02ab7d4f6fd5b22
devleejb <devleejb@gmail.com>
2024-02-08 22:49:48 +0900

    Add python dependency

--- Formula/cllm.rb
+++ Formula/cllm.rb
@@ -4,6 +4,9 @@ class Cllm < Formula
   url "https://github.com/dev-backpack/cllm/releases/download/v0.1.0/cllm-0.1.0.tar.gz" 
   sha256 "d18f230616dfea375c93789fe11cbc78bcd97520fe26329ff7f1d928f6092a85"
   version "0.1.0"
+  license "MIT"
+
+  depends_on "python@3.10"
 
   def install
     virtualenv_install_with_resources

10bdeccedb1f9360269cb7a948a14216b716ef2c
devleejb <devleejb@gmail.com>
2024-02-08 22:47:22 +0900

    Add `cllm.rb`
    Co-authored-by: Chanism <ykdy3951@users.noreply.github.com>

---
+++ Formula/cllm.rb
@@ -0,0 +1,11 @@
+class Cllm < Formula
+  desc "Empower your CLI experience with a command search tool driven by LLM magic!" 
+  homepage "https://github.com/dev-backpack/cllm" 
+  url "https://github.com/dev-backpack/cllm/releases/download/v0.1.0/cllm-0.1.0.tar.gz" 
+  sha256 "d18f230616dfea375c93789fe11cbc78bcd97520fe26329ff7f1d928f6092a85"
+  version "0.1.0"
+
+  def install
+    virtualenv_install_with_resources
+  end
+end

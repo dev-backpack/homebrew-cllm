9a92a04e6b67ae6377068ae4f02c1de4a301617e
devleejb <devleejb@gmail.com>
2024-02-12 00:01:37 +0900

    Update to 0.1.1

--- Formula/cllm.rb
+++ Formula/cllm.rb
@@ -1,9 +1,9 @@
 class Cllm < Formula
   desc "Empower your CLI experience with a command search tool driven by LLM magic!" 
   homepage "https://github.com/dev-backpack/cllm" 
-  url "https://github.com/dev-backpack/cllm/releases/download/v0.1.0/cllm-0.1.0.tar.gz" 
-  sha256 "d18f230616dfea375c93789fe11cbc78bcd97520fe26329ff7f1d928f6092a85"
-  version "0.1.0"
+  url "https://github.com/dev-backpack/cllm/releases/download/v0.1.1/cllm-0.1.1.tar.gz" 
+  sha256 "718c29878193127ddcf5f1d73f6f47ee261dba219d1de437aa61f6a374c449cc"
+  version "0.1.1"
   license "MIT"
 
   depends_on "poetry" => :build

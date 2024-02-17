e449adf1aa4cde84b5c628da23dd5fa4da884eed
devleejb <devleejb@gmail.com>
2024-02-17 21:37:01 +0900

    Update cllm.rb

--- Formula/cllm.rb
+++ Formula/cllm.rb
@@ -29,6 +29,11 @@ class Cllm < Formula
     sha256 "105254a8b04934f0bc84e9c24eb360a591aaf6535c9def5f29d92af107a9bf57"
   end
 
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
+    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
+  end
+
   def install
     virtualenv_install_with_resources
   end

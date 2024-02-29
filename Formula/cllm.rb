ff1e7f77c57eb528755251f235251ba5283064be
devleejb <devleejb@gmail.com>
2024-02-29 14:46:55 +0900

    Remove redundant version

--- Formula/cllm.rb
+++ Formula/cllm.rb
@@ -2,7 +2,6 @@ class Cllm < Formula
   desc "Empower your CLI experience with a command search tool driven by LLM magic!"
   homepage "https://github.com/dev-backpack/cllm"
   url "https://github.com/dev-backpack/cllm/archive/refs/tags/v0.1.1.tar.gz"
-  version "0.1.1"
   sha256 "2f0713c46b9e977e80f0263c7030a60899c4c4020049e2c02b2157844d299103"
   license "MIT"
 
